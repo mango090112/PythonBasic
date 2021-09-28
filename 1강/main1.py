5b76cdfeb404e315e06fe956722bf93e3e615d54
mango090112 <91520666+mango090112@users.noreply.github.com>
2021-09-28 16:20:08 +0900

    Create main1.py

---
+++ 1강/main1.py
@@ -0,0 +1,50 @@
+print('철수가 인사한다. "안녕?"')
+print ("철수가 인사한다. \"안녕?\"")
+print("나의 '첫 코딩' 입니다.")
+print("오늘은", "일요일")
+print("월요일", "화요일", "수요일", "목요일", "금요일")
+print("월요일 화요일 수요일 목요일 금요일")
+print("네이버;카카오;삼성")
+print("네이버", "카카오", "삼성", sep=";")
+print("월요일", "화요일", "수요일", "목요일", "금요일", sep=",")
+a=123
+print(a)
+a=-178
+print(a)
+a=0
+print(a)
+a=1.2
+print(a)
+a=-3.45
+print(a)
+a=100098
+print(a)
+a=20090112
+print(a)
+# 주석입니다.
+# 8진수
+#a=0o177
+print(a)
+# 16진수
+#a=0x8ff
+print(a)
+a=100
+b=9
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a+a*b-3)
+print(a**b)
+print(a%b)
+print(a//b)
+a=80
+b=75
+c=55
+print((a+b+c)/3)
+a=87
+b=3
+print(a//b)
+print(a%b)
+a=13
+print(a%2)
